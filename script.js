6148b6904cca65502ee242e8526fd9af222dff75
kiplangat15000 <kiplangatdennis99@gmail.com>
2021-03-14 00:45:40 +0300

    Assigning values to varriables

---
+++ script.js
@@ -0,0 +1,15 @@
+function submit() {
+    var shedA = document.getElementById("milk_a").Value;
+    var shedB = document.getElementById("milk_b").Value;
+    var shedC = document.getElementById("milk_c").value;
+    var shedD = document.getElementById("milk_c").value;
+    var year = 12;
+    var monthsD = 30;
+    var weekD = 7;
+    var price = 45;
+
+
+
+
+    
+}
